8732fc85ddb254d8d99a27ef155f6c5093c7d33e
Taswy <taswisy@gmail.com>
2019-03-13 11:18:33 +0800

    19.03.13.01

--- batch.py
+++ batch.py
@@ -1,7 +1,7 @@
 from kamene.all import *
 
-pcap_path = "c:\\Users\\taswi\\Desktop"
-filename = str.join("\\", [pcap_path, "demo.pcap"])
+pcap_path = "/home/taswy/Code/Python/batch-wireshark"
+filename = str.join("/", [pcap_path, "demo.pcap"])
 packets = rdpcap(filename)
 
 
@@ -9,4 +9,4 @@ print(packets[672].time - packets[671].time)
 #print(packets[671].__dict__)
 print(packets[671].len/(packets[672].time - packets[671].time))
 p = packets[671]
-print(packets[671].tcp.
+print(p)
